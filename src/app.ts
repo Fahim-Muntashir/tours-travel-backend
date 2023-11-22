4a3def3be42611cc4008e65194681faa3a2b5c51
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-22 20:53:24 +0600

    added basic config

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+
+
+const app:Application = express()
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to Murir DIm Tour Nad Travel"
+  })
+})
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app"
+
+// getting-started.js
+import mongoose from "mongoose";
+import config from "./config";
+
+async function server() {
+ try {
+     await mongoose.connect(config.database_url_local);
+     console.log("connected to mongodb");
+     app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+    })
+ } catch (error) {
+     console.log(error);
+ }
+  }
+
+server().catch(err => console.log(err));
